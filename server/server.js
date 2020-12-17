e80a62b8e6325b856f64337ad300b8b4e36a8876
sathwikajoginipalli <71289487+sathwikajoginipalli@users.noreply.github.com>
2020-12-17 09:30:37 -0500

    gitignore

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ const compression = require('compression');
 
 // app.use(cors(corsOptions));
 app.use(cors());
+app.use(compression());
 
 const url= "mongodb://localhost:27017/personal_budget";
 let user_data= require("./app/routes/user_route");
